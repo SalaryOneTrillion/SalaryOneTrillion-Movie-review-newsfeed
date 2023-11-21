3a4f5764c746146016cd285d640b300d5d29be9e
wonowonow <wonow_@naver.com>
2023-11-21 20:07:27 +0900

    init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# SalaryOneTrillion-Movie-review-newsfeed
---
+++ src/main/java/com/sparta/salaryonetrillionmoviereviewnewsfeed/entity/ReviewEntity.java
@@ -0,0 +1,9 @@
+package com.sparta.salaryonetrillionmoviereviewnewsfeed.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class ReviewEntity {
+
+}
