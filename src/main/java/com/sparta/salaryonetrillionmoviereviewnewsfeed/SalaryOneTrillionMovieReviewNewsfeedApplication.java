99d49212f8b868011257811fc4c6d1f3c881065d
wonowonow <wonow_@naver.com>
2023-11-21 20:06:04 +0900

    init commit

---
+++ src/main/java/com/sparta/salaryonetrillionmoviereviewnewsfeed/SalaryOneTrillionMovieReviewNewsfeedApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.salaryonetrillionmoviereviewnewsfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaryOneTrillionMovieReviewNewsfeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaryOneTrillionMovieReviewNewsfeedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/salaryonetrillionmoviereviewnewsfeed/SalaryOneTrillionMovieReviewNewsfeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.salaryonetrillionmoviereviewnewsfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaryOneTrillionMovieReviewNewsfeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
