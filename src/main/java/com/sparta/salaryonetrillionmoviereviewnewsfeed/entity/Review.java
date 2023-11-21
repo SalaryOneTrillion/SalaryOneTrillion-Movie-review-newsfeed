b3f4a0f68672a3549e7679ba841176648b9bdcbd
woo <wsh1322@naver.com>
2023-11-21 22:11:07 +0900

    feat : Review Entity 추가

---
+++ src/main/java/com/sparta/salaryonetrillionmoviereviewnewsfeed/entity/Review.java
@@ -0,0 +1,35 @@
+package com.sparta.salaryonetrillionmoviereviewnewsfeed.entity;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_id")
+    private Long id;
+
+    @Column(name = "review_content")
+    private String content;
+
+    @CreatedDate
+    @Column(updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime modifiedAt;
+
+    @Column(name = "review_like")
+    private int reviewLike;
+
+    @Column(name = "review_movie_rating")
+    private float movieRating;
+}
