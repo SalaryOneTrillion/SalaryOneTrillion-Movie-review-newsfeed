8e17b0b00efc792fe2f66bea61cae4026cf95c52
m2bi <junghigh78@naver.com>
2023-11-22 11:15:43 +0900

    feat : category업ã데이트

---
+++ src/main/java/com/sparta/salaryonetrillionmoviereviewnewsfeed/entity/Category.java
@@ -0,0 +1,33 @@
+package com.sparta.salaryonetrillionmoviereviewnewsfeed.entity;
+
+import jakarta.annotation.Nullable;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id", length = 255, nullable = false)
+    @NotEmpty
+    @Nullable
+    private long categoty_id;
+
+    @Column(name = "category_country", length = 30, nullable = false)
+    private String category_country;
+
+    @Column(name = "category_genre", length = 20, nullable = false)
+    private String category_genre;
+
+}
