3230ab098dc48277376cb1c97b6dbba3f1eb16ce
m2bi <junghigh78@naver.com>
2023-11-23 16:33:15 +0900

    카테고리추가

---
+++ src/main/java/com/sparta/salaryonetrillionmoviereviewnewsfeed/entity/Category.java
@@ -0,0 +1,27 @@
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id", length = 255, nullable = false)
+    private long categoty_id;
+
+    @Column(name = "category_country", length = 30, nullable = false)
+    private String category_country;
+
+    @Column(name = "category_genre", length = 20, nullable = false)
+    private String category_genre;
+
+}
