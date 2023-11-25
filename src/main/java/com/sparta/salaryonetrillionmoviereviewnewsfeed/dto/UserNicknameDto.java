8be2033f8a4440bf93e41739f85230b2b8015e83
wonowonow <123870616+wonowonow@users.noreply.github.com>
2023-11-25 17:17:40 +0900

    Update UserNicknameDto.java

--- src/main/java/com/sparta/salaryonetrillionmoviereviewnewsfeed/dto/UserNicknameDto.java
+++ src/main/java/com/sparta/salaryonetrillionmoviereviewnewsfeed/dto/UserNicknameDto.java
@@ -6,6 +6,6 @@ import lombok.Getter;
 @Getter
 public class UserNicknameDto {
 
-    @Pattern(regexp = "[^a-zA-Z0-9/]{2,10}", message = "한글, 알파벳 대,소문자, 숫자로 구성해주세요")
+    @Pattern(regexp = "[a-zA-Z0-9/가-힣]{2,10}", message = "한글, 알파벳 대,소문자, 숫자로 구성해주세요")
     String nickname;
 }
