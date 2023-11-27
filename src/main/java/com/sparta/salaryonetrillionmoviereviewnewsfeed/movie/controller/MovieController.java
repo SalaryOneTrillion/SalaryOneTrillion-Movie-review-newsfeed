d5b913f4294e1ef4c1eb3ed4218747662dcabb66
wonowonow <123870616+wonowonow@users.noreply.github.com>
2023-11-27 19:09:36 +0900

    refactor : RequestMapping 추가

--- src/main/java/com/sparta/salaryonetrillionmoviereviewnewsfeed/movie/controller/MovieController.java
+++ src/main/java/com/sparta/salaryonetrillionmoviereviewnewsfeed/movie/controller/MovieController.java
@@ -8,16 +8,18 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/movies")
 public class MovieController {
 
     private final MovieService movieService;
 
-    @GetMapping("/movies")
+    @GetMapping
     public ResponseEntity<Page<MoviePageResponseDto>> getMovies(
             @RequestParam int page,
             @RequestParam int size,
@@ -27,7 +29,7 @@ public class MovieController {
         return ResponseEntity.status(200).body(movieService.getMovies(page-1, size, sortBy, isAsc));
     }
 
-    @GetMapping("/movies/{movieId}")
+    @GetMapping("/{movieId}")
     public ResponseEntity<MovieResponseDto> getMovie(@PathVariable Long movieId) {
 
         return ResponseEntity.status(200).body(movieService.getMovie(movieId));
